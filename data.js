2b121455939734c6e792a2a99c39ca00dd88b220
jeanne-v <126341674+jeanne-v@users.noreply.github.com>
2024-01-24 11:56:43 +0100

    Add definitive images

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
 const cardsData = [
-    {
-        imgUrl: "istockphoto-1422593766-1024x1024.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "istockphoto-1010910478-1024x1024.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "pexels-melike-benli-10074791.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "istockphoto-667376706-1024x1024.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "pexels-elina-sazonova-1927210.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "istockphoto-1131157601-1024x1024.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "pexels-secret-garden-931154.jpg",
-        id: ""
-    },
-    {
-        imgUrl: "istockphoto-1175182873-1024x1024.jpg",
-        id: ""
-    }
-]
+  {
+    imgUrl: "florem-ipsum-bouquet-1.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-2.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-3.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-4.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-5.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-6.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-7.jpg",
+    id: "",
+  },
+  {
+    imgUrl: "florem-ipsum-bouquet-8.jpg",
+    id: "",
+  },
+];
 
 export default cardsData;
